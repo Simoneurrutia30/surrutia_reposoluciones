1cdaacbd26b7d1d80529f6da855cdba026c2c77e
Bioinformatica <bioinformatica@icb-probook>
2018-05-03 18:15:09 -0300

    Se ha agregado el lab 4

--- Lab4/Ejercicio2.c
+++ Lab4/Ejercicio2.c
@@ -9,7 +9,7 @@
 #include <stdio.h>
 int dinero_que_debe_pagar ()
 {
-	int huevos=8, amigos=3;
+	float huevos=8, amigos=3;
 	float huevos_a_comer_por_persona;
 	int valor_de_cada_huevo=10;
 	int huevos_de_juan=5, huevos_de_manuel=3;
@@ -18,7 +18,7 @@ int dinero_que_debe_pagar ()
 	  
 	huevos_a_comer_por_persona=huevos/amigos;
 	
-	printf("\n La cantidad de huevos que come cada amigo: %.0f", huevos_a_comer_por_persona);
+	printf(" La cantidad de huevos que come cada amigo: %.1f", huevos_a_comer_por_persona);
 	
 	porcentajedejuan=huevos_de_juan-huevos_a_comer_por_persona;
 	
@@ -29,8 +29,8 @@ int dinero_que_debe_pagar ()
     total_a_pagar_a_manuel=porcentajedemanuel*valor_de_cada_huevo;
     
 
-    printf("\n Antonio le debe pagar a juan: %.0f", total_a_pagar_a_juan);
-    printf("\n Antonio le debe pagar a manuel: %.0f", total_a_pagar_a_manuel);
+    printf("\n Antonio le debe pagar a juan: %.1f", total_a_pagar_a_juan);
+    printf("\n Antonio le debe pagar a manuel: %.1f", total_a_pagar_a_manuel);
     
     return 0;
 }
